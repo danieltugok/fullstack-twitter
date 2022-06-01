63ac6b21b8c2bbe37389e130a6070712ffc8a3b3
Daniel Kogut <danieltugok@gmail.com>
2022-06-01 08:00:17 -0500

    components input and README

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-react, tailwindcss, heroicon, 
+<h1 align="center">
+  <!-- <img alt="Rocket.Q" title="Rocket.Q" src=".github/rocketq.png" width="220px" /> -->
+</h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Project</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">License</a>
+</p>
+
+<p align="center">
+ <img src="https://img.shields.io/static/v1?label=PRs&message=welcome&color=49AA26&labelColor=000000" alt="PRs welcome!" />
+
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="Twitter" src=".github/capa.png" width="100%">
+</p>
+
+## 🚀 Tecnologies
+
+This project was developed with:
+
+- HTML5
+- CSS3
+- React
+- JavaScript
+- Tailwind CSS
+- Heroicon
+
+## 💻 Project
+
+The main thing in this project is to study more about react with tailwind css cloning the Twitter app idea.
+
+## 🔖 Layout
+
+You can see the Layout through [this link](https://www.figma.com/file/ojPxxBponimVoAGb9Z8t24/UI-Twitter-Clone?node-id=0%3A1). It is necessary to have an [Figma](https://figma.com) account to access.
+
+## :memo: License
+
+This project is MIT License.
+
+## 🌵 Run the project 
+
+
+        //install dependencies
+        npm install
+
+        //Create DB
+        npm run dev
+
+
+Developed with ❤️ by Daniel Kogut
--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,38 +1,86 @@
-import {HeartIcon} from '@heroicons/react/outline'
+import { useState } from "react";
+import { HeartIcon } from "@heroicons/react/outline";
 
+const MAX_TWITTER_CHAR = 250;
 
+function TweetForm() {
 
+  const [text, setText] = useState("");
 
-function Tweet({ name, username, avatar, children}){
+  function changeText(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div className="border-b border-silver p-4 space-y-6">
+      <div className="flex p-4 space-x-5">
+        <img src="/src/imgs/avatar.png" className="w-7" alt="" />
+        <h1 className="font-bold text-xl">Inicial Page</h1>
+      </div>
+
+      <form action="" className="pl-12 text-lg flex flex-col">
+        <textarea
+          placeholder="What's happening?"
+          type="text"
+          name="text"
+          value={text}
+          className="bg-transparent outline-none disabled:opacity-50"
+          onChange={changeText}
+        />
+        <div className="flex justify-end items-center space-x-3">
+          <span className="text-sm">
+            <span>{text.length}</span> /{" "}
+            <span className="text-birdBlue"> {MAX_TWITTER_CHAR}</span>
+          </span>
+          <button 
+            disabled={text.length > MAX_TWITTER_CHAR}
+            className="bg-birdBlue px-5 py-2 rounded-full disabled:opacity-50"
+          >Tweet</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function Tweet({ name, username, avatar, children }) {
   return (
     <div className="flex space-x-3 p-4 border-b border-silver ">
       <div>
         <img src={avatar} class="tweet_img" alt="" />
       </div>
       <div className="space-y-1">
-        <span className="font-bold text-sm">{name}</span>{' '}
+        <span className="font-bold text-sm">{name}</span>{" "}
         <span className="font-bold text-silver">@{username}</span>
-
         <p>{children}</p>
-        <div className='flex space-x-1 text-silver text-sm items-center'>
-        <HeartIcon className='w-6 stroke-1'/>
-        <span>1.2k</span>
+        <div className="flex space-x-1 text-silver text-sm items-center">
+          <HeartIcon className="w-6 stroke-1" />
+          <span>1.2k</span>
         </div>
       </div>
     </div>
-  )
+  );
 }
-
 
 export const App = () => {
   return (
     <>
-    <Tweet name="Elon Musk" username="elonmusk" avatar="/src/imgs/avatar.png" >
-      Let make Twitter maximum fun!
-    </Tweet>
-    <Tweet name="Daniel Kogut" username="danielkogut" avatar="/src/imgs/avatar.png" >
-      Let make Twitter maximum awesome!
-    </Tweet>
+      <TweetForm />
+      <div>
+        <Tweet
+          name="Elon Musk"
+          username="elonmusk"
+          avatar="/src/imgs/avatar.png"
+        >
+          Let make Twitter maximum fun!
+        </Tweet>
+        <Tweet
+          name="Daniel Kogut"
+          username="danielkogut"
+          avatar="/src/imgs/avatar.png"
+        >
+          Let make Twitter maximum awesome!
+        </Tweet>
+      </div>
     </>
-  )
-}
+  );
+};
